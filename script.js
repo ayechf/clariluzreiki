af202f0f597ba6296089102fbc084c2ed4acc02b
Aye <a_ch_f@live.com.ar>
2023-11-03 21:51:41 -0300

    Actualizacion 5 Commit Entrega Final

---
+++ script.js
@@ -0,0 +1,32 @@
+// Sliders
+(function(){
+    
+    const sliders = [...document.querySelectorAll('.cuerpo_opiniones')];
+    const botonAdelante = document.querySelector('#adelante');
+    const botonAtras = document.querySelector('#atras');
+    let valor;   
+
+    botonAdelante.addEventListener('click', ()=>{
+        cambioPosicion(1); 
+    });
+
+    botonAtras.addEventListener('click', ()=>{
+        cambioPosicion(-1);
+    });
+
+    const cambioPosicion = (add)=>{
+        const actualOpinion = document.querySelector('.cuerpo_opiniones_muestra').dataset.id;
+        valor = Number(actualOpinion);
+        valor+= add;
+
+
+        sliders[Number(actualOpinion)-1].classList.remove('cuerpo_opiniones_muestra');
+        if(valor === sliders.length+1 || valor === 0){
+            valor = valor === 0 ? sliders.length  : 1;
+        }
+
+        sliders[valor-1].classList.add('cuerpo_opiniones_muestra');
+
+    }
+
+})();
